0b664dc6ce6859d24efa4697307c697130394652
Alejandro Feria Gonzalez <aferiag@unal.edu.co>
2025-07-21 20:59:54 -0500

    fix

--- code/server.js
+++ code/server.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+var cors = require('cors')
 const app = express();
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "50mb" }));
 
 // sincronizar la base de datos
 app.use("/db", require("./_helpers/db.js"));
@@ -21,4 +27,7 @@ app.use("/detalle_items", require("./detalle_items/detalle_items.routes"));
 
 //acarreos
 app.use("/acarreos", require("./acarreos/acarreos.routes"));
+
+app.use("/cuidadores", require("./cuidadores/cuidadores.routes"));
+
 module.exports = app;
